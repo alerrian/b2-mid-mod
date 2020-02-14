e40f798e5b6505badf6dda47a74ccd2051067b5d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-14 10:19:17 -0700

    Add user story 1

--- spec/features/studios/studio_index_spec.rb
+++ spec/features/studios/studio_index_spec.rb
@@ -3,23 +3,61 @@ require 'rails_helper'
 RSpec.describe 'as a user', type: :feature do
   describe 'when I visit the index' do
     before :each do
-      @studio1 = Studio.create!(
-        name: 'Fox'
-      )
-      @studio2 = Studio.create!(
+      @disney = Studio.create!(
         name: 'Disney'
       )
-      @studio3 = Studio.create!(
+      @lucasfilm = Studio.create!(
         name: 'Lucasfilm'
+      )
+
+      @disney_movie1 = Movie.create!(
+        name: 'Snow White',
+        year: '1935',
+        genre: 'cartoon',
+        studio_id: @disney.id
+      )
+
+      @lucasfilm_movie1 = Movie.create!(
+        name: 'The Phantom Menace',
+        year: '1999',
+        genre: 'Sci-Fi',
+        studio_id: @lucasfilm.id
+      )
+
+      @lucasfilm_movie2 = Movie.create!(
+        name: 'A New Hope',
+        year: '1978',
+        genre: 'Sci-Fi',
+        studio_id: @lucasfilm.id
+      )
+
+      @disney_movie2 = Movie.create!(
+        name: 'The Force Awakens',
+        year: '2014',
+        genre: 'Sci-Fi',
+        studio_id: @disney.id
       )
     end
 
     it 'can see all of the movie studios' do
       visit '/studios'
 
-      expect(page).to have_content('Fox')
       expect(page).to have_content('Disney')
       expect(page).to have_content('Lucasfilm')
+    end
+
+    it 'can see a list of movies under each studio' do
+      visit '/studios'
+
+      within "#studio_#{@disney.id}" do
+        expect(page).to have_content('Snow White')
+        expect(page).to have_content('The Force Awakens')
+      end
+
+      within "#studio_#{@lucasfilm.id}" do
+        expect(page).to have_content('The Phantom Menace')
+        expect(page).to have_content('A New Hope')
+      end
     end
   end
 end
